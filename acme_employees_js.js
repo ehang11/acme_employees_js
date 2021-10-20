d3aa61636c0563c8f4489bdc1aaca39a3be5e868
Eric Hang <ehang11@gmail.com>
2021-10-20 14:37:47 -0400

    first commit

---
+++ acme_employees_js.js
@@ -0,0 +1,189 @@
+
+const employees = [
+  { id: 1, name: 'moe'},
+  { id: 2, name: 'larry', managerId: 1},
+  { id: 4, name: 'shep', managerId: 2},
+  { id: 3, name: 'curly', managerId: 1},
+  { id: 5, name: 'groucho', managerId: 3},
+  { id: 6, name: 'harpo', managerId: 5},
+  { id: 8, name: 'shep Jr.', managerId: 4},
+  { id: 99, name: 'lucy', managerId: 1}
+];
+
+const spacer = (text)=> {
+  if(!text){
+    return console.log('');
+  }
+  const stars = new Array(5).fill('*').join('');
+  console.log(`${stars} ${text} ${stars}`);
+}
+
+spacer('findEmployeeByName Moe')
+// given a name and array of employees, return employee
+console.log(findEmployeeByName('moe', employees));//{ id: 1, name: 'moe' }
+spacer('')
+
+//DONE@@@@@@@@@  Function: 1 findEmployeeByName
+ function findEmployeeByName(name, arr) {
+    return arr.find(x => x.name === name); 
+ }
+
+
+
+spacer('findManagerFor Shep Jr.')
+//given an employee and a list of employees, return the employee who is the manager
+console.log(findManagerFor(findEmployeeByName('shep Jr.', employees), employees));//{ id: 4, name: 'shep', managerId: 2 }
+spacer('')
+
+//DONE@@@@@@@@ Function 2: findManagerFor 
+function findManagerFor(name, arr) {
+
+    //prints managerId: 4
+    return arr.filter(x => x.id===name.managerId);
+}
+       
+
+
+
+spacer('findCoworkersFor Larry')
+
+//given an employee and a list of employees, return the employees who report to the same manager
+console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));/*
+[ { id: 3, name: 'curly', managerId: 1 },
+  { id: 99, name: 'lucy', managerId: 1 } ]
+*/
+spacer('');
+
+//DONE@@@@@@@@@@@ Function 3: findCoworkersFor
+function findCoworkersFor(name, arr) {
+    return arr.filter(x => x.managerId===name.managerId)
+}
+
+spacer('findManagementChain for moe')
+//given an employee and a list of employees, return a the management chain for that employee. 
+//The management chain starts from the employee with no manager with the passed in employees manager 
+console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));//[  ]
+spacer('');
+
+//DONE@@@@@@@@@@@ Function 4: findManagementChainForEmployee
+function findManagementChainForEmployee(name, arr) {
+   
+        let reportArr=[]
+   if (name.hasOwnProperty('managerId'))  {
+     let reportTree=name.name
+     //let arrTest=(findManagerFor(findEmployeeByName(reportTree, arr), arr));
+       //findManagerFor(name, arr);
+  
+       //reportArr= (findManagerFor(name, arr));
+       //return reportArr.find(x => x.name);
+       //return reportArr
+       //return findManagementChainForEmployee(findManagerFor(name, arr), arr);
+    }
+
+        else {
+            return [];
+           }
+           
+    }
+
+     
+     
+        
+
+      
+   
+
+   
+
+
+spacer('findManagementChain for shep Jr.')
+console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));/*
+[ { id: 1, name: 'moe' },
+  { id: 2, name: 'larry', managerId: 1 },
+  { id: 4, name: 'shep', managerId: 2 }]
+*/
+spacer('');
+
+//@@@@@@@@@@@ Function 5: findManagementChainForEmployee 
+
+
+
+
+spacer('generateManagementTree')
+//given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
+console.log(JSON.stringify(generateManagementTree(employees), null, 2));
+
+//@@@@@@@@@@@  Function 6: generateManagementTree 
+
+
+
+/*
+{
+  "id": 1,
+  "name": "moe",
+  "reports": [
+    {
+      "id": 2,
+      "name": "larry",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 4,
+          "name": "shep",
+          "managerId": 2,
+          "reports": [
+            {
+              "id": 8,
+              "name": "shep Jr.",
+              "managerId": 4,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "curly",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 5,
+          "name": "groucho",
+          "managerId": 3,
+          "reports": [
+            {
+              "id": 6,
+              "name": "harpo",
+              "managerId": 5,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 99,
+      "name": "lucy",
+      "managerId": 1,
+      "reports": []
+    }
+  ]
+}
+*/
+spacer('');
+
+spacer('displayManagementTree')
+//given a tree of employees, generate a display which displays the hierarchy
+displayManagementTree(generateManagementTree(employees));/*
+moe
+-larry
+--shep
+---shep Jr.
+-curly
+--groucho
+---harpo
+-lucy
+*/
+
+//@@@@@@@@@@@  Function 7: displayManagementTree 
